66d19a3e2cc906c0ea3eb72dd7486852c667bd08
Anshu Yadav <yadavanshu70012@gmail.com>
2026-03-18 18:47:28 +0000

    save

--- src/SimpleExecutor.java
+++ src/SimpleExecutor.java
@@ -1,30 +1,51 @@
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class SimpleExecutor {
-    private Thread worker;
-    private BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(10);
+    private final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(10);
+    private final Thread[] workers;
+    volatile boolean isShutdown = false;
 
     SimpleExecutor() {
-        worker = new Thread(()->{
-            while(true){
-                Runnable task = queue.poll();
-                if(task != null){
-                    task.run();
+
+        workers = new Thread[4];
+        for (int i = 0; i < workers.length; i++) {
+
+            workers[i] = new Thread(()->{
+                while(!Thread.currentThread().isInterrupted()) {
+                    try {
+                        Runnable task = queue.take();
+                        task.run();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        if(isShutdown && queue.isEmpty()) {
+                            break;
+                        }
+                    }
                 }
-            }
-        });
-        worker.start();
+            });
+            workers[i].start();
+        }
     }
 
     public void submit(Runnable task) {
+
+        if(isShutdown) {
+            throw new IllegalStateException("Executor has been shutdown");
+        }
+
         try {
             queue.put(task);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            Thread.currentThread().interrupt();
         }
+    }
 
+    public void shutdown() {
+        isShutdown = true;
+
+        for(Thread worker : workers) {
+            worker.interrupt();
+        }
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,25 @@
 void main() {
     SimpleExecutor ex = new SimpleExecutor();
 
-    for (int i = 0; i < 100; i++) {
-        int x = i;
-        new Thread(() -> {
-            ex.submit(() -> IO.println(x));
-        }).start();
+//    for (int i = 0; i < 100; i++) {
+//        int x = i;
+//        new Thread(() -> {
+//            ex.submit(() -> IO.println(x));
+//        }).start();
+//    }
+
+    for(int j = 0; j < 10; j++) {
+        ex.submit(() -> {
+            long sum = 0;
+            for(int i = 0; i < 10000; i++) {
+                sum = sum + i;
+            }
+            System.out.println(Thread.currentThread().getName() + " " + sum);
+        });
     }
+
+
+//    ex.shutdown();
 
     try {
         Thread.sleep(3000);
